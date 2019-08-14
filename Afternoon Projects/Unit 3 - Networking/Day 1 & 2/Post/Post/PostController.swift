7f7d38fed3d998b3aeefe4d90861c81f98183a76
Sambo <flatheadcatfish9@yahoo.com>
2019-08-13 20:50:40 -0600

    Alert controller and posting

--- Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/PostController.swift
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/PostController.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class PostController: Codable {
     
-    static let sharedInstance = PostController()
+//    static let sharedInstance = PostController()
     
     let baseURL = URL(string: "https://devmtn-posts.firebaseio.com/posts")
     
@@ -18,11 +18,29 @@ class PostController: Codable {
     
     var posts: [Post] = []
     
-    func fetchPosts(completion: @escaping() -> Void) {
+    func fetchPosts(reset: Bool = true, completion: @escaping() -> Void) {
+        
+//        var reset: Bool = true
+        
+        let queryEndInterval = reset ? Date().timeIntervalSince1970 : posts.last?.timestamp ?? Date().timeIntervalSince1970
         
         guard let unwrappedURL = baseURL else { return }
         
-        let getterEndpoint = unwrappedURL.appendingPathExtension("json")
+        let urlParameters = [
+            "orderBy": "\"timestamp\"",
+            "endAt": "\(queryEndInterval)",
+            "limitToLast": "15",
+        ]
+        
+        let queryItems = urlParameters.compactMap( { URLQueryItem(name: $0.key, value: $0.value) } )
+        
+        var urlComponents = URLComponents(url: unwrappedURL, resolvingAgainstBaseURL: true)
+        
+        urlComponents?.queryItems = queryItems
+        
+        guard let url = urlComponents?.url else { return }
+        
+        let getterEndpoint = url.appendingPathExtension("json")
         
         var request = URLRequest(url: getterEndpoint)
         
@@ -47,14 +65,64 @@ class PostController: Codable {
                 let postsDictionary = try jsonDecoder.decode([String: Post].self, from: data)
                 var posts = postsDictionary.compactMap({ $0.value })
                 posts.sort(by: { $0.timestamp > $1.timestamp })
-                self.posts = posts
+                if reset == true {
+                    self.posts = posts
+                } else {
+                    self.posts.append(contentsOf: posts)
+                }
+                
+                
                 completion()
                 
             } catch let error {
                 print(error)
+                completion()
+                return
             }
             
             }
+        dataTask.resume()
+    }
+    
+    func addNewPostWith(username: String, text: String, completion: @escaping() -> Void) {
+        
+        let post = Post(text: text, username: username)
+        var postData: Data?
+        
+        do {
+            let jsonEncoder = JSONEncoder()
+            let je = try jsonEncoder.encode(post)
+            postData = je
+        } catch let error {
+            print(error)
+            return
+        }
+        
+        guard let unwrappedBaseURL = baseURL else { return }
+        let postEndpoint = unwrappedBaseURL.appendingPathExtension("json")
+        
+        var request = URLRequest(url: postEndpoint)
+        
+        request.httpBody = postData
+        request.httpMethod = "POST"
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("\(error) : \(error.localizedDescription)")
+                completion()
+                return
+            }
+            
+            guard let data = data else { return }
+            let printableData = String(data: data, encoding: .utf8)
+            self.posts.append(post)
+            print(printableData)
+            
+            self.fetchPosts(completion: {
+                completion()
+            })
+            
+        }
         dataTask.resume()
     }
     
--- Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/PostListViewController.swift
+++ Afternoon Projects/Unit 3 - Networking/Day 1 & 2/Post/Post/PostListViewController.swift
@@ -12,11 +12,14 @@ class PostListViewController: UIViewController {
 
     @IBOutlet weak var postTableViewOutlet: UITableView!
     
+    // TAke out shared instance and replace with postController.
+    var postController = PostController()
     var refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
+        refreshControl.addTarget(self, action: #selector(refreshControlPulled), for: .valueChanged)
         postTableViewOutlet.refreshControl = refreshControl
         
         postTableViewOutlet.estimatedRowHeight = 45
@@ -24,30 +27,36 @@ class PostListViewController: UIViewController {
         postTableViewOutlet.delegate = self
         postTableViewOutlet.dataSource = self
         
-        PostController.sharedInstance.fetchPosts { 
+//        PostController.sharedInstance.fetchPosts {
+        postController.fetchPosts {
 
             DispatchQueue.main.async {
                 
                 
-                self.postTableViewOutlet.reloadData()
-                self.refreshControlPulled()
+//                self.postTableViewOutlet.reloadData()
+//                self.refreshControlPulled()
                 self.reloadTableView()
             }
 
         }
     }
     
+    // MARK: - Actions
+    @IBAction func addButtonPressed(_ sender: Any) {
+        presentNewPostAlert()
+    }
+    
     // MARK: - Methods
     
     @objc func refreshControlPulled() {
-        refreshControl.addTarget(self, action: #selector(refreshControlPulled), for: .valueChanged)
         
-        PostController.sharedInstance.fetchPosts {
+        postController.fetchPosts {
+//        PostController.sharedInstance.fetchPosts {
             DispatchQueue.main.async {
 //                self.refreshControl.endRefreshing()
 //                self.postTableViewOutlet.refreshControl?.endRefreshing()
                 UIApplication.shared.isNetworkActivityIndicatorVisible = true
-                self.postTableViewOutlet.refreshControl?.endRefreshing()
+                self.refreshControl.endRefreshing()
                 
 //                self.reloadTableView()
             }
@@ -62,30 +71,88 @@ class PostListViewController: UIViewController {
         }
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func presentNewPostAlert() {
+        
+        let alert = UIAlertController(title: "Hello", message: "Add your username and message below.", preferredStyle: .alert)
+        
+        
+        
+        let alertAction = UIAlertAction(title: "Submit your post", style: .default) { (_) in
+            guard let message = (alert.textFields![0] as UITextField).text,
+                let username = (alert.textFields![1] as UITextField).text,
+                !message.isEmpty,
+                !username.isEmpty else {
+                    // error
+                    self.presentErrorAlert()
+                    return
+            }
+            
+            self.postController.addNewPostWith(username: username, text: message, completion: {
+            
+//            PostController.sharedInstance.addNewPostWith(username: username, text: message, completion: {
+                DispatchQueue.main.async {
+                    
+                    self.reloadTableView()
+                }
+            })
+        }
+        
+        alert.addAction(alertAction)
+        
+        let dismissAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(dismissAction)
+        
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "Username"
+        }
+        alert.addTextField { (textfield) in
+            textfield.placeholder = "Message"
+        }
+        present(alert, animated: true, completion: nil)
+//        reloadTableView()
     }
-    */
-
+    
+    func presentErrorAlert() {
+        let alert = UIAlertController(title: "Ooops", message: "You need a valid username and complete message", preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "Cancel", style: .cancel, handler: {(action) in
+            self.presentNewPostAlert() })
+        alert.addAction(dismissAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 extension PostListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PostController.sharedInstance.posts.count
+//        return PostController.sharedInstance.posts.count
+        return postController.posts.count
+
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
 
-        let post = PostController.sharedInstance.posts[indexPath.row]
+//        let post = PostController.sharedInstance.posts[indexPath.row]
+        let post = postController.posts[indexPath.row]
         cell.textLabel?.text = post.text
         cell.detailTextLabel?.text = post.username
         return cell
     }
     
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        if indexPath.row > (PostController.sharedInstance.posts.count - 1) {
+//            PostController.sharedInstance.fetchPosts(reset: false) {
+//                self.reloadTableView()
+//            }
+        if indexPath.row > (postController.posts.count - 1) {
+            postController.fetchPosts(reset: false) {
+                self.reloadTableView()
+            }
+        }
+        return
+    }
+    
 }
+
+
